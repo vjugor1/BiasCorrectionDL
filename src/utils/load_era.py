8c9da5545d722eda61370be532b9b5f2039c7886
d.taniushkina <d.taniushkina@skoltech.ru>
2024-03-20 13:09:50 +0300

    docker update

---
+++ src/utils/load_era.py
@@ -0,0 +1,58 @@
+import xarray as xr
+import os
+from tqdm import tqdm
+import logging
+import sys
+
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+
+@hydra.main(version_base=None, config_path=os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "configs"), config_name="bias_correction.yaml")
+def run_zarr_load(cfg: DictConfig):#vars, years, PATH):
+    # Define time period for analysis
+    YEAR_START = cfg.load["year_start"]
+    YEAR_END = cfg.load["year_end"]
+
+    # Years range
+    years = range(YEAR_START, YEAR_END + 1)
+
+    # Path to save files
+    PATH = cfg.load["era_save_path"]
+    os.makedirs(PATH, exist_ok=True)
+
+    vars = cfg.load["single_level_vars"]
+    
+    logging.info("ERA5 download script started.")
+    # Access ERA5 from WeatherBench2
+    data = xr.open_zarr(
+        "gs://weatherbench2/datasets/era5/1959-2022-full_37-1h-0p25deg-chunk-1.zarr-v2"
+    )
+    logging.info("Zarr opened.")
+    
+    # Transform longitude to fit with other data
+    # data.coords["longitude"] = (data.coords["longitude"] + 180) % 360 - 180
+    # data = data.sortby(data.longitude)
+
+    # Loop over variables
+    logging.info("Starting download...")
+    for year in tqdm(years):
+        for var in [*vars]:
+            logging.info(f"Variable: {var}; Year: {year}")
+            data_sel = data[var].sel(time=str(year))
+            
+            if var=="2m_temperature":
+                data_sel = data_sel.resample(time='1D').mean(dim='time')
+            elif var=="total_precipitation":
+                data_sel = data_sel.resample(time='1D').sum(dim='time')
+            else:
+                print(f"For variable {var} procedure not implemented")
+
+            # Save to Zarr
+            # Adjust the path and potentially add storage options if saving to cloud storage
+            zarr_path = os.path.join(PATH, "{}_{}.zarr".format(var, year))
+            data_sel.to_zarr(zarr_path, mode='w')
+    logging.info("Download finished.")
+
+if __name__ == "__main__":
+    run_zarr_load()
