e4abbb915aea3973de8f80c005da5ac761ba6a69
Escape142 <escape756@gmail.com>
2024-03-27 12:15:25 +0000

    local_adaptation_of_climate_learn

--- era5_era5_baselines.py
+++ era5_era5_baselines.py
@@ -1,29 +1,67 @@
+# Standard library
+from argparse import ArgumentParser
+
 # Third party
-import climate_learn as cl
+import torch
+from src.climate_learn.data import IterDataModule
+from src.climate_learn import load_downscaling_module
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import (
+    RichModelSummary,
+    RichProgressBar,
+)
 
-# Set up data
-in_vars = out_vars = [
-    "2m_temperature",
+torch.set_float32_matmul_precision("medium")
 
-]
-dm = cl.data.IterDataModule(
-    "downscaling",
-    inp_root_dir="/app/data/ClimateLearn/processed/ERA5/5.625_24sh",
-    out_root_dir="/app/data/ClimateLearn/processed/ERA5/2.8125_24sh",
+parser = ArgumentParser()
+parser.add_argument(
+    "era5_low_res_dir",
+    type=str,
+    default="/app/data/ClimateLearn/processed/ERA5/5.625_24sh",
+)
+parser.add_argument(
+    "era5_high_res_dir",
+    type=str,
+    default="/app/data/ClimateLearn/processed/ERA5/2.8125_24sh",
+)
+args = parser.parse_args()
+
+dm = IterDataModule(
+    task="downscaling",
+    inp_root_dir=args.era5_low_res_dir,
+    out_root_dir=args.era5_high_res_dir,
     in_vars=in_vars,
     out_vars=out_vars,
     subsample=1,
-    batch_size=32,
+    batch_size=256,
+    buffer_size=10000,
     num_workers=4,
-    )
+)
 dm.setup()
 
 # Set up baseline models
-nearest = cl.load_downscaling_module(data_module=dm, architecture="nearest-interpolation")
-bilinear = cl.load_downscaling_module(data_module=dm, architecture="bilinear-interpolation")
+nearest = load_downscaling_module(data_module=dm, architecture="nearest-interpolation")
+bilinear = load_downscaling_module(
+    data_module=dm, architecture="bilinear-interpolation"
+)
 
+callbacks = [
+    RichProgressBar(),
+    RichModelSummary(max_depth=1),
+]
 # Evaluate baselines (no training needed)
-trainer = pl.Trainer(accelerator="cpu")
-trainer.test(nearest, dm)
-trainer.test(bilinear, dm)
+trainer = pl.Trainer(
+    accelerator="cpu",
+    callbacks=callbacks,
+)
+
+
+# Perform validation and testing for each model
+for model, model_name in zip(
+    [nearest, bilinear], ["nearest-interpolation", "bilinear-interpolation"]
+):
+    print("Validating model:", model_name)
+    trainer.validate(model, dataloaders=dm)
+
+    print("Testing model:", model_name)
+    trainer.test(model, dataloaders=dm)
--- era5_era5_dl.py
+++ era5_era5_dl.py
@@ -1,10 +1,9 @@
+# Standard library
+from argparse import ArgumentParser
+
 # Third party
 import torch
-import climate_learn as cl
-from climate_learn.data.processing.era5_constants import (
-    PRESSURE_LEVEL_VARS,
-    DEFAULT_PRESSURE_LEVELS,
-)
+from src.climate_learn import LitModule, IterDataModule, load_downscaling_module
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import (
     EarlyStopping,
@@ -15,42 +14,70 @@ from pytorch_lightning.callbacks import (
 from pytorch_lightning.loggers.tensorboard import TensorBoardLogger
 
 torch.set_float32_matmul_precision("medium")
-dm = cl.data.IterDataModule(
-    "downscaling",
-    inp_root_dir="/app/data/ClimateLearn/processed/ERA5/5.625_24sh",
-    out_root_dir="/app/data/ClimateLearn/processed/ERA5/2.8125_24sh",
-    in_vars=["2m_temperature"],
-    out_vars=["2m_temperature"],
+
+parser = ArgumentParser()
+parser.add_argument(
+    "era5_low_res_dir",
+    type=str,
+    default="/app/data/ClimateLearn/processed/ERA5/5.625_24sh",
+)
+parser.add_argument(
+    "era5_high_res_dir",
+    type=str,
+    default="/app/data/ClimateLearn/processed/ERA5/2.8125_24sh",
+)
+parser.add_argument(
+    "architecture", type=str, choices=["resnet", "unet", "vit"], default="vit"
+)
+parser.add_argument("--summary_depth", type=int, default=1)
+parser.add_argument("--max_epochs", type=int, default=100)
+parser.add_argument("--patience", type=int, default=5)
+parser.add_argument("--gpu", type=int, default=[0, 1, 2, 3])
+parser.add_argument("--checkpoint", default=None)
+args = parser.parse_args()
+
+
+# Set up data
+in_vars = out_vars = [
+    "2m_temperature",
+]
+
+dm = IterDataModule(
+    task="downscaling",
+    inp_root_dir=args.era5_low_res_dir,
+    out_root_dir=args.era5_high_res_dir,
+    in_vars=in_vars,
+    out_vars=out_vars,
     subsample=1,
     batch_size=256,
     buffer_size=10000,
-    num_workers=64,
+    num_workers=4,
 )
 dm.setup()
 
 # Set up deep learning model
-model = cl.load_downscaling_module(
+model = load_downscaling_module(
     data_module=dm,
-    architecture="vit",
+    architecture=args.architecture,
     train_loss="mse",
-    val_loss=["mse"],
-    test_loss=["mse"],
+    val_loss=["rmse", "pearson", "mean_bias", "mse"],
+    test_loss=["rmse", "pearson", "mean_bias", "mse"],
     train_target_transform=None,
-    val_target_transform=[None],
-    test_target_transform=[None],
+    val_target_transform=["denormalize", "denormalize", "denormalize", None],
+    test_target_transform=["denormalize", "denormalize", "denormalize", None],
 )
 
 # Setup trainer
 pl.seed_everything(0)
 default_root_dir = (
-    f"data/ClimateLearn/processed/downscaling-ERA5-ERA5/vit_downscaling_t2m_24sh"
+    f"data/ClimateLearn/processed/downscaling-ERA5-ERA5/vit_downscaling_t2m"
 )
 logger = TensorBoardLogger(save_dir=f"{default_root_dir}/logs")
 early_stopping = "val/mse:aggregate"
 callbacks = [
-    # RichProgressBar(),
-    RichModelSummary(max_depth=1),
-    EarlyStopping(monitor=early_stopping, patience=5),
+    RichProgressBar(),
+    RichModelSummary(max_depth=args.summary_depth),
+    EarlyStopping(monitor=early_stopping, patience=args.patience),
     ModelCheckpoint(
         dirpath=f"{default_root_dir}/checkpoints",
         monitor=early_stopping,
@@ -64,12 +91,26 @@ trainer = pl.Trainer(
     callbacks=callbacks,
     default_root_dir=default_root_dir,
     accelerator="gpu",
-    devices=-1,
-    max_epochs=100,
-    strategy="ddp",
+    devices=[0],
+    max_epochs=args.max_epochs,
+    strategy="auto",
     precision="16-mixed",
 )
 
 # Train and evaluate model from scratch
-trainer.fit(model, datamodule=dm)
-trainer.test(model, datamodule=dm, ckpt_path="best")
+if args.checkpoint is None:
+    trainer.fit(model, datamodule=dm)
+    trainer.test(model, datamodule=dm, ckpt_path="best")
+# Evaluate saved model checkpoint
+else:
+    model = LitModule.load_from_checkpoint(
+        args.checkpoint,
+        net=model.net,
+        optimizer=model.optimizer,
+        lr_scheduler=None,
+        train_loss=None,
+        val_loss=None,
+        test_loss=model.test_loss,
+        test_target_tranfsorms=model.test_target_transforms,
+    )
+    trainer.test(model, datamodule=dm)
