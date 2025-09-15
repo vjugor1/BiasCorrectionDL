1e24735ffae4157d5e8dc81c542d4fcbd3493d21
DariaTan <bien.ou.rien@gmail.com>
2025-09-15 10:29:49 +0300

    run pred for all seeds

--- src/benchmark/make_predictions_for_ensemble.py
+++ src/benchmark/make_predictions_for_ensemble.py
@@ -4,6 +4,9 @@ import torch
 import tqdm
 import pickle
 
+import sys
+sys.path.append('/app')
+
 from hydra import compose, initialize
 from cmip6_cmip6_dl_base_model import main
 
@@ -11,10 +14,10 @@ FACTOR = 2
 CONFIG_NAME_TRAIN = "cmip6-cmip6"
 CONFIG_NAME_INFERENCE = f"cmip6-cmip6_factor{FACTOR}_ens"
 
-with initialize(version_base=None, config_path="configs/inference"):
+with initialize(version_base=None, config_path="../../configs/inference"):
     cfg_inference = compose(config_name=CONFIG_NAME_INFERENCE)
 
-with initialize(version_base=None, config_path="configs/train"):
+with initialize(version_base=None, config_path="../../configs/train"):
     cfg_train = compose(config_name=CONFIG_NAME_TRAIN)
 
 train_ens_data = {}
@@ -23,7 +26,7 @@ test_ens_data = {}
 i=1
 models = cfg_inference.models_base
 seed = cfg_train.training.seed
-device = 'cuda:4'
+device = 'cuda:2'
 
 for model in models:
     version=cfg_inference[model][1].versions[i]
@@ -37,45 +40,6 @@ for model in models:
     model_module.eval().to(device)
     dm.setup()
 
-    # y_pred_train = []
-    # with torch.inference_mode():
-    #     for batch in tqdm.tqdm(dm.predict_dataloader()):
-    #         x, _, _, _ = batch
-    #         y_pred_train.append(model_module(x.to(device, non_blocking=True)))
-
-    # y_pred_val = []
-    # with torch.inference_mode():
-    #     for batch in dm.val_dataloader():
-    #         x, _, _, _ = batch
-    #         y_pred_val.append(model_module(x.to(device, non_blocking=True)))
-
-    # y_pred_test = []
-    # with torch.inference_mode():
-    #     for batch in dm.test_dataloader():
-    #         x, _, _, _ = batch
-    #         y_pred_test.append(model_module(x.to(device, non_blocking=True)))
-
-    # train_ens_data[model + '_' + str(seed)] = torch.cat(y_pred_train, dim=0) #.to('cpu')
-    # val_ens_data[model + '_' + str(seed)] = torch.cat(y_pred_val, dim=0) #. to('cpu')
-    # test_ens_data[model + '_' + str(seed)] = torch.cat(y_pred_test, dim=0) #.to('cpu')
-
-    # print('saving train')
-    # torch.save(train_ens_data, f'train_ens_data_factor_{FACTOR}_{model}_{seed}.pt')
-    # # with open(f'train_ens_data_factor_4_{model}_{seed}.pkl', 'wb') as f:
-    # #     pickle.dump(train_ens_data, f)
-    # print('done')
-
-    # print('saving val')
-    # torch.save(train_ens_data, f'val_ens_data_factor_{FACTOR}_{model}_{seed}.pt')
-    # # with open(f'val_ens_data_factor_4_{model}_{seed}.pkl', 'wb') as f:
-    # #     pickle.dump(val_ens_data, f)
-    # print('done')
-
-    # print('saving test')
-    # torch.save(train_ens_data, f'test_ens_data_factor_{FACTOR}_{model}_{seed}.pt')
-    # # with open(f'test_ens_data_factor_4_{model}_{seed}.pkl', 'wb') as f:
-    # #     pickle.dump(test_ens_data, f)
-    # print('done')
     for stage, dataloader in zip(['train', 'test', 'val'], 
                                 [dm.train_dataloader(), dm.test_dataloader(), dm.val_dataloader()]
                                 ):
